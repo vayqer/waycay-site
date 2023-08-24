7048ec741af8ff235494eac2c0b9c0a0bfee6e8b
Lukas Hakkarainen <lukas@vayqer.se>
2023-08-24 21:21:56 +0200

    Updates

--- _app/immutable/chunks/paths.ff192e96.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1ay4c2l)==null?void 0:s.base)??"/waycay-site";var a;const t=((a=globalThis.__sveltekit_1ay4c2l)==null?void 0:a.assets)??e;export{t as a,e as b};
